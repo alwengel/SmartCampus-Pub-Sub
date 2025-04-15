f6ae905b9ad75be007862ac7bc7968c863a9e557
Catrovitch <ale.engelhardt@outlook.com>
2025-04-15 10:55:23 +0300

    id added

--- schema.sql
+++ schema.sql
@@ -1,4 +1,6 @@
-CREATE TABLE publications (publication_id , timestamp , deveui , temperature , humidity , light , motion , co2 , battery , sound_avg , sound_peak , moisture , pressure , acceleration_x , acceleration_y , acceleration_z , rssi , lsnr , chan , port , rfch , seqn , fcnt , sensor_type , floor , location , publication , subscription_matches , timestamp_unix );
-
 CREATE TABLE subscriptions (id INTEGER, complexity TEXT, nlp_subscription TEXT, publication_match_count INTEGER, sql_subscription TEXT);
+
+CREATE TABLE publications (id INTEGER PRIMARY KEY AUTOINCREMENT, publication_id , timestamp , deveui , temperature , humidity , light , motion , co2 , battery , sound_avg , sound_peak , moisture , pressure , acceleration_x , acceleration_y , acceleration_z , rssi , lsnr , chan , port , rfch , seqn , fcnt , sensor_type , floor , location , publication , subscription_matches , timestamp_unix );
+
+CREATE TABLE sqlite_sequence(name,seq);
 
